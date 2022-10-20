7a8caf80090caef73f4cca0f75d0eda12d48c30b
Salmanu Usman <usalmanu1@gmail.com>
2022-10-20 13:04:03 -0700

    Printf Group Project

---
+++ testing/flag_tests.c
@@ -0,0 +1,158 @@
+#include <stdio.h>
+#include "main.h"
+
+int main()
+{
+	int len, len2;
+
+	len = _printf("%%+d: [%+d]\n", 10);
+	len2 = printf("%%+d: [%+d]\n", 10);
+	printf("len: %d\n", len);
+	printf("len2: %d\n", len2);
+	if (len != len2)
+	{
+		printf("Test 1: Lengths differ.\n");
+		fflush(stdout);
+		return (1);
+	}
+
+	len = _printf("%% i: [% i]\n", 11);
+	len2 = printf("%% i: [% i]\n", 11);
+	printf("len: %d\n", len);
+	printf("len2: %d\n", len2);
+	if (len != len2)
+	{
+		printf("Test 2: Lengths differ.\n");
+		fflush(stdout);
+		return (1);
+	}
+
+	len = _printf("%%#d: [%#d]\n", 12);
+	len2 = printf("%%#d: [%#d]\n", 12);
+	printf("len: %d\n", len);
+	printf("len2: %d\n", len2);
+	if (len != len2)
+	{
+		printf("Test 3: Lengths differ.\n");
+		fflush(stdout);
+		return (1);
+	}
+	len = _printf("%%#x: [%#x]\n", 13);
+	len2 = printf("%%#x: [%#x]\n", 13);
+	printf("len: %d\n", len);
+	printf("len2: %d\n", len2);
+	if (len != len2)
+	{
+		printf("Test 4: Lengths differ.\n");
+		fflush(stdout);
+		return (1);
+	}
+	len = _printf("%%#x: [%#x]\n", 0);
+	len2 = printf("%%#x: [%#x]\n", 0);
+	printf("len: %d\n", len);
+	printf("len2: %d\n", len2);
+	if (len != len2)
+	{
+		printf("Test 5: Lengths differ.\n");
+		fflush(stdout);
+		return (1);
+	}
+
+	len = _printf("%%#o: [%#o]\n", 14);
+	len2 = printf("%%#o: [%#o]\n", 14);
+	printf("len: %d\n", len);
+	printf("len2: %d\n", len2);
+	if (len != len2)
+	{
+		printf("Test 6: Lengths differ.\n");
+		fflush(stdout);
+		return (1);
+	}
+
+	len = _printf("%%#o: [%#o]\n", 0);
+	len2 = printf("%%#o: [%#o]\n", 0);
+	printf("len: %d\n", len);
+	printf("len2: %d\n", len2);
+	if (len != len2)
+	{
+		printf("Test 7: Lengths differ.\n");
+		fflush(stdout);
+		return (1);
+	}
+
+	len = _printf("%%+x: [%+x]\n", 15);
+	len2 = printf("%%+x: [%+x]\n", 15);
+	printf("len: %d\n", len);
+	printf("len2: %d\n", len2);
+	if (len != len2)
+	{
+		printf("Test 8: Lengths differ.\n");
+		fflush(stdout);
+		return (1);
+	}
+	printf("----Test width with invalid and/or no specifiers----\n");
+
+
+	len = _printf("%10 ");
+	printf("\n");
+	len2 = printf("%10 ");
+	printf("\n");
+	printf("len: %d\n", len);
+	printf("len2: %d\n", len2);
+	if (len != len2)
+	{
+		printf("Test 9: Lengths differ.\n");
+		fflush(stdout);
+		return (1);
+	}
+	len = _printf("% % % % ");
+	printf("\n");
+	len2 = printf("% % % % ");
+	printf("\n");
+	printf("len: %d\n", len);
+	printf("len2: %d\n", len2);
+	if (len != len2)
+	{
+		printf("Test 10: Lengths differ.\n");
+		fflush(stdout);
+		return (1);
+	}
+	len = _printf("% ");
+	printf("\n");
+	len2 = printf("% ");
+	printf("\n");
+	printf("len: %d\n", len);
+	printf("len2: %d\n", len2);
+	if (len != len2)
+	{
+		printf("Test 11: Lengths differ.\n");
+		fflush(stdout);
+		return (1);
+	}
+
+	len = _printf("%y %");
+	printf("\n");
+	len2 = printf("%y %");
+	printf("\n");
+	printf("len: %d\n", len);
+	printf("len2: %d\n", len2);
+	if (len != len2)
+	{
+		printf("Test 12: Lengths differ.\n");
+		fflush(stdout);
+		return (1);
+	}
+	len = _printf("%y %+");
+	printf("\n");
+	len2 = printf("%y %+");
+	printf("\n");
+	printf("len: %d\n", len);
+	printf("len2: %d\n", len2);
+	if (len != len2)
+	{
+		printf("Test 13: Lengths differ.\n");
+		fflush(stdout);
+		return (1);
+	}
+	return (0);
+}
---
+++ testing/width_tests.c
@@ -0,0 +1,120 @@
+#include <stdio.h>
+#include "main.h"
+
+int main()
+{
+	int len;
+	int len2;
+
+	len = _printf("%%*d, 10, 100: [%*d]\n", 10, 100);
+	len2 = printf("%%*d, 10, 100: [%*d]\n", 10, 100);
+	printf("len: %d\n", len);
+	printf("len2: %d\n", len2);
+	if (len != len2)
+	{
+		printf("Test 1: Lengths differ.\n");
+		fflush(stdout);
+		return (1);
+	}
+
+	len = _printf("%%*d|%%i, 20, 15, 30: [%*d|%i]\n", 20, 15, 30);
+	len2 = printf("%%*d|%%i: 20, 15, 30: [%*d|%i]\n", 20, 15, 30);
+	printf("len: %d\n", len);
+	printf("len2: %d\n", len2);
+	if (len != len2)
+	{
+		printf("Test 2: Lengths differ.\n");
+		fflush(stdout);
+		return (1);
+	}
+
+	len = _printf("%%*d %%*i, 5, 20, 10, 30: [%*d %*i]\n", 5, 20, 10, 30);
+	len2 = printf("%%*d %%*i, 5, 20, 10, 30: [%*d %*i]\n", 5, 20, 10, 30);
+	printf("len: %d\n", len);
+	printf("len2: %d\n", len2);
+	if (len != len2)
+	{
+		printf("Test 3: Lengths differ.\n");
+		fflush(stdout);
+		return (1);
+	}
+
+	len = _printf("%%10s: [%10s]\n", "hello");
+	len2 = printf("%%10s: [%10s]\n", "hello");
+	printf("len: %d\n", len);
+	printf("len2: %d\n", len2);
+	if (len != len2)
+	{
+		printf("Test 4: Lengths differ.\n");
+		fflush(stdout);
+		return (1);
+	}
+
+	len = _printf("%%10c: [%10c]\n", 'c');
+	len2 = printf("%%10c: [%10c]\n", 'c');
+	printf("len: %d\n", len);
+	printf("len2: %d\n", len2);
+	if (len != len2)
+	{
+		printf("Test 5: Lengths differ.\n");
+		fflush(stdout);
+		return (1);
+	}
+
+	len = _printf("%%10d, 100: [%10d]\n", 100);
+	len2 = printf("%%10d, 100: [%10d]\n", 100);
+	printf("len: %d\n", len);
+	printf("len2: %d\n", len2);
+	if (len != len2)
+	{
+		printf("Test 6: Lengths differ.\n");
+		fflush(stdout);
+		return (1);
+	}
+
+	len = _printf("%%10o, 100: [%10o]\n", 100);
+	len2 = printf("%%10o, 100: [%10o]\n", 100);
+	printf("len: %d\n", len);
+	printf("len2: %d\n", len2);
+	if (len != len2)
+	{
+		printf("Test 7: Lengths differ.\n");
+		fflush(stdout);
+		return (1);
+	}
+
+	len = _printf("%%10x, 100: [%10x]\n", 100);
+	len2 = printf("%%10x, 100: [%10x]\n", 100);
+	printf("len: %d\n", len);
+	printf("len2: %d\n", len2);
+	if (len != len2)
+	{
+		printf("Test 8: Lengths differ.\n");
+		fflush(stdout);
+		return (1);
+	}
+
+	len = _printf("%%20p: [%20p]\n", (void *)0x7ffe637541f0);
+	len2 = printf("%%20p: [%20p]\n", (void *)0x7ffe637541f0);
+	printf("len: %d\n", len);
+	printf("len2: %d\n", len2);
+	if (len != len2)
+	{
+		printf("Test 9: Lengths differ.\n");
+		fflush(stdout);
+		return (1);
+	}
+
+	len = _printf("%%2c: [%2c]\n", '\0');
+	len2 = printf("%%2c: [%2c]\n", '\0');
+	printf("len: %d\n", len);
+	printf("len2: %d\n", len2);
+	if (len != len2)
+	{
+		printf("Test 10: Lengths differ.\n");
+		fflush(stdout);
+		return (1);
+	}
+
+	return (0);
+}
